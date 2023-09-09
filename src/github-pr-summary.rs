2ca714670e5d17bd49695fe6117a5834fcedb0c7
nanomolina <nanomolinacav@gmail.com>
2023-09-09 04:07:33 -0300

    Fix Pymobi url typo

--- src/github-pr-summary.rs
+++ src/github-pr-summary.rs
@@ -118,7 +118,7 @@ async fn handler(
         }
     } else {
         // PR OPEN or Trigger phrase: create a new comment
-        match issues.create_comment(pull_number, "Hello, I am a [code review bot](https://github.com/PyMobi/github-pr-summary/) on [PyMobi](https://PyMobi/).\n\nIt could take a few minutes for me to analyze this PR. Relax, grab a cup of coffee and check back later. Thanks!").await {
+        match issues.create_comment(pull_number, "Hello, I am a [code review bot](https://github.com/PyMobi/github-pr-summary/) on [PyMobi](https://github.com/PyMobi).\n\nIt could take a few minutes for me to analyze this PR. Relax, grab a cup of coffee and check back later. Thanks!").await {
             Ok(comment) => {
                 comment_id = comment.id;
             }
@@ -197,7 +197,7 @@ async fn handler(
     }
 
     let mut resp = String::new();
-    resp.push_str("Hello, I am a [code review bot](https://github.com/PyMobi/github-pr-summary/) on [PyMobi](https://PyMobi/). Here are my reviews of code commits in this PR.\n\n------\n\n");
+    resp.push_str("Hello, I am a [code review bot](https://github.com/PyMobi/github-pr-summary/) on [PyMobi](https://github.com/PyMobi). Here are my reviews of code commits in this PR.\n\n------\n\n");
     if reviews.len() > 1 {
         log::debug!("Sending all reviews to OpenAI for summarization");
         let co = ChatOptions {
