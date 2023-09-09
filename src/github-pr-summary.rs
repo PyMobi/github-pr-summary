6145deb51c853a5a76a1a830091c44361232fb78
nanomolina <nanomolinacav@gmail.com>
2023-09-09 10:33:56 -0300

    #2 Update chatgpt prompt to reach a max size

--- src/github-pr-summary.rs
+++ src/github-pr-summary.rs
@@ -168,10 +168,10 @@ async fn handler(
     }
 
     let chat_id = format!("PR#{pull_number}");
-    let system = &format!("You are an experienced software developer. You will act as a reviewer for a GitHub Pull Request titled  \"{}\". 
-                           Please provide a brief but comprehensive review of the changes in the pull request, highlighting key concerns, potential improvements, and suggestions.
-                           Remember to stay within a character limit of \"{}\" for the review.
-                          ", title, CHAR_SOFT_LIMIT);
+    let system = &format!("You are an experienced software developer. You will act as a reviewer for a GitHub Pull Request titled \"{}\". \
+                        Please provide a brief but comprehensive review of the changes in the pull request, highlighting key concerns, potential improvements, and suggestions. \
+                        Remember to stay within a character limit of \"{}\" for the review.",
+                        title, CHAR_SOFT_LIMIT);
     let mut openai = OpenAIFlows::new();
     openai.set_retry_times(3);
 
